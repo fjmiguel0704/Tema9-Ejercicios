dcedcac1b14febc91877f34c8424e599dec84948
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-05-17 14:35:58 +0200

    Terminar

---
+++ Tema9-Ejercicios/src/ejercicios/Ejercicio4.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		int numeros;
+		
+		TreeMap<Integer, Integer> numero = new TreeMap<Integer, Integer>();
+		TreeMap<Integer, Integer> estrellas = new TreeMap<Integer, Integer>();
+		
+		Scanner read = new Scanner (System.in);
+		
+		System.out.println("Introduce tus 5 primeros números del euromillón: ");
+		numeros=read.nextInt();
+	}
+
+}
