9a13e927ad2bbcb4d9142c7492a5fa760291db3c
fmigg <fmigg@Fernando>
2023-05-22 23:32:39 +0200

    Terminado y comentado

--- Tema9-Ejercicios/src/ejercicios/Ejercicio4.java
+++ Tema9-Ejercicios/src/ejercicios/Ejercicio4.java
@@ -15,34 +15,50 @@ public class Ejercicio4 {
 		// Valor inicial para las claves de estrellas
 		int valorEstre = 1;
 
-		// Creamos la coleccion TreeSet para agregar datos a ella y ordenar sus datos
+		// Creamos la coleccion TreeSet para los números del eutomillon, agregar datos a
+		// ella y ordenar sus datos
 		TreeMap<Integer, Integer> numero = new TreeMap<Integer, Integer>();
+		// Creamos la coleccion TreeSet para las estrellas del eutomillon, agregar datos
+		// a ella y ordenar sus datos
 		TreeMap<Integer, Integer> estrellas = new TreeMap<Integer, Integer>();
 
 		// Scanner para leer por teclado
 		Scanner read = new Scanner(System.in);
 
-		// Mientras
+		// Mientras el contador sea menor a 7, se incrementará
 		while (cont <= 7) {
+			// Si contador está entre 0 y 4 (ambos incluidos), se pedirán los 5 numeros del
+			// euromillon
 			if (cont >= 0 && cont <= 4) {
 				System.out.println("Introduce tus 5 primeros números del euromillón: ");
 				numeros = read.nextInt();
+				// Y si ya se encuentra ese número, se incrementará su valor dependiendo del
+				// numero de veces que se encuentre
 				if (numero.containsKey(numeros)) {
 					numero.replace(numeros, valorNum++);
 				}
+				// Añadimos a la coleccion el número y su valor
 				numero.put(numeros, valorNum);
 			}
+
+			// Si el contador está entre 5 y 6 (ambos incluidos, se pedirán las 2 estrellas
+			// del euromillon
 			if (cont >= 5 && cont <= 6) {
 				System.out.println("Introduce las 2 estrellas de tu euromillón: ");
 				numeros = read.nextInt();
+				// Y si ya se encuentra esa estrella, se incrementará su valor dependiendo del
+				// numero de veces que se encuentre
 				if (estrellas.containsKey(numeros)) {
 					estrellas.replace(numeros, valorEstre++);
 				}
+				// Añadimos a la coleccion la estrella y su valor
 				estrellas.put(numeros, valorEstre);
 			}
+			// Incrementamos el contador
 			cont++;
 		}
 
+		// Imprimimos la serie completa del euromillón(numero y estrellas)
 		System.out.println(numero + " " + estrellas);
 
 	}
--- Tema9-Ejercicios/src/ejercicios/Ejercicio5.java
+++ Tema9-Ejercicios/src/ejercicios/Ejercicio5.java
@@ -6,17 +6,26 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
 	public static void main(String[] args) {
+		// Guardará la opción elegida del menú
 		int opc;
+		// Guardará el nombre del producto
 		String nomProduct;
+		// Guardará el precio del producto
 		double precio;
 
-		//
+		// Creamos una coleccion para guardar los productos y su precio, sin importar
+		// el orden de estos
 		HashMap<String, Double> productos = new HashMap<String, Double>();
+		// Creamos una coleccion para almacenar el stock de cada uno de los productos,
+		// pudiendo ser en muchos de ellos el mismo stock
 		ArrayList<Integer> stock = new ArrayList<Integer>();
 
+		// Scanner para leer por teclado
 		Scanner read = new Scanner(System.in);
 
+		// Haz esto mientras ->
 		do {
+			// Mostramos el menú de opciones y le pedimos una opcion al usuario
 			System.out.println();
 			System.out.println("PRODUCTOS");
 			System.out.println("----------------------------");
@@ -30,6 +39,9 @@ public class Ejercicio5 {
 			read.nextLine();
 
 			switch (opc) {
+			// Si es 1, agregamos un nuevo producto. Pedimos el nombre y precio del
+			// producto, y si no existe lo añadimos
+			// a la coleccion, si existe mostramos un mensaje de error
 			case 1:
 				System.out.println("Introduce el nombre del prodcuto: ");
 				nomProduct = read.next();
@@ -44,6 +56,10 @@ public class Ejercicio5 {
 				}
 
 				break;
+
+			// Si es 2, eliminamos un producto. Pedimos el nombre del producto, y si
+			// existe lo eliminamos de la coleccion, si no existe mostramos un mensaje de
+			// error
 			case 2:
 				System.out.println("Introduce el nombre del prodcuto a quitar: ");
 				nomProduct = read.next();
@@ -55,16 +71,19 @@ public class Ejercicio5 {
 				}
 
 				break;
+			// Si es 3, mostramos la lista de productos con su precio y stock
 			case 3:
 				System.out.println(productos + " " + stock);
 				break;
+			// Si es 0, salimos del programa
 			case 0:
 				System.out.println("Saliendo...");
 				break;
 			default:
+				// Si la opcion es distinta a las anteriores, mostramos un mensaje de error
 				System.out.println("Opción incorrecta");
 			}
-		} while (opc != 0);
+		} while (opc != 0); // ->Mientras la opcion sea distinta a 0
 
 	}
 }
